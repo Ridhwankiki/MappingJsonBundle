33149c822730b3879dc9bcfe440bbb9d07bcb210
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-24 08:45:32 +0700

    Initial Commit

---
+++ MappingJsonBundle/MappingJsonBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingJsonBundleApp.swift
+//  MappingJsonBundle
+//
+//  Created by MacBook Pro on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MappingJsonBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
